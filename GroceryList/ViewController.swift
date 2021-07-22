6f1b1aa5735864a99b13762a072516320af981e6
Matthew Poelsterl <mpoelsterl1217@gmail.com>
2021-07-22 12:04:15 -0500

    Built MVP
    App builds and displays sample data from an array in a table view

--- GroceryList/ViewController.swift
+++ GroceryList/ViewController.swift
@@ -7,13 +7,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    //Globals
+    var groceryList = ["Apples", "Carrots", "Barbecue Sauce"]
+    
+    //Outlets
+    @IBOutlet weak var groceryListTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        groceryListTableView.delegate = self
+        groceryListTableView.dataSource = self
     }
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return groceryList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "protoCell")
+        cell?.textLabel?.text = groceryList[indexPath.row]
+        return cell!
+    }
+    
+    //Actions
 
 }
 
